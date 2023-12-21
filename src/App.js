e54dd2d4deb4e8402730bd5108cc94751ddd79dd
alu0101124506 <alu0101124506@ull.edu.es>
2023-12-21 01:02:49 +0000

    Prueba

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css'
+import { LoginSignUp } from './components/auth/login.component'
+
+function App() {
+  return (
+    <div>
+      <LoginSignUp />
+    </div>
+  )
+}
+
+export default App
