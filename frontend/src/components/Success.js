d81de8cbf52a876971b9f747ff85bccd219ebfad
Carla Oval Torres <alu0101036694@ull.edu.es>
2024-05-19 17:01:10 +0100

    estilo corregido

--- frontend/src/components/Success.js
+++ frontend/src/components/Success.js
@@ -2,10 +2,9 @@ import React from 'react';
 
 export function Success() {
   return (
-      <div className="bg-white p-8 rounded shadow-md max-w-md w-full text-center bg-green-100">
+    <div className="bg-white p-8 rounded shadow-md max-w-md w-full text-center bg-green-100">
         <h1 className="text-4xl font-bold text-green-800 mb-4">Login Exitoso</h1>
         <p className="text-lg text-green-700">¡Has iniciado sesión correctamente!</p>
-   
     </div>
   );
 }
