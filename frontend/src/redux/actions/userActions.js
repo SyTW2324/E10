5187462a11b781dde83edde1eedaca6e4b86d86e
Carla Oval Torres <alu0101036694@ull.edu.es>
2024-05-19 16:38:34 +0100

    url del backend añadida

--- frontend/src/redux/actions/userActions.js
+++ frontend/src/redux/actions/userActions.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
+// Definir la URL base del backend
+const BASE_URL = 'http://localhost:5000';
+
 // Acción para registrar un usuario
 export const registerUser = createAsyncThunk(
   'user/register',
   async (userData, { rejectWithValue }) => {
     try {
-      const response = await axios.post('/api/register', userData);
+      const response = await axios.post(`${BASE_URL}/api/register`, userData);
       return response.data;
     } catch (error) {
       return rejectWithValue(error.response.data);
@@ -19,7 +22,7 @@ export const loginUser = createAsyncThunk(
   'user/login',
   async (userData, { rejectWithValue }) => {
     try {
-      const response = await axios.post('/api/login', userData);
+      const response = await axios.post(`${BASE_URL}/api/login`, userData);
       return response.data;
     } catch (error) {
       return rejectWithValue(error.response.data);
