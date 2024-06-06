f26db7a411f8626a3854644ec53dd107f2cd7075
alu0101124506 <alu0101124506@ull.edu.es>
2024-06-06 18:43:56 +0100

    Actualizacion apariencia registro e inicio de sesion exitoso

--- frontend/src/components/RegisterSuccess.js
+++ frontend/src/components/RegisterSuccess.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 export function RegisterSuccess() {
+  const user = useSelector(state => state.user.user);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate('/login');
+    }
+  }, [user, navigate]);
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-b from-purple-800 to-purple-900">
-      <div className="bg-white p-8 rounded shadow-md text-center">
-        <h1 className="text-4xl font-bold text-green-800 mb-4">Registro exitoso</h1>
-        <p className="text-lg text-green-700 mb-4">¡Te has registrado correctamente!</p>
-        <Link to="/login" className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700">
-          Ir al Log In
-        </Link>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-400">
+      <div className="max-w-md w-full space-y-8 bg-gray-900 p-8 rounded shadow-md text-center">
+        <h1 className="text-4xl font-bold text-green-600 mb-4">Registro exitoso</h1>
+        <p className="text-gray-300 text-lg mb-6">¡Te has registrado correctamente!</p>
+        <button className="w-full py-2 px-4 bg-gradient-to-b from-blue-600 to-blue-800 text-white rounded-md hover:from-blue-700 hover:to-blue-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500" onClick={() => navigate('/songs')}>
+          Ir a la página de canciones
+        </button>
       </div>
     </div>
   );
