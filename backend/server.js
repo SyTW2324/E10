9222660d08f61568618696beb86d271b247aa3ae
Carlos Pío Reyes <alu0101132945@ull.edu.es>
2024-06-10 20:43:02 +0100

    server

--- backend/server.js
+++ backend/server.js
@@ -129,11 +129,11 @@ app.put('/api/songs/:id', async (req, res) => {
 });
 
 // Servir archivos estáticos de React
-app.use(express.static(path.join(__dirname, 'frontend/build')));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 // Manejar todas las rutas, redirigiéndolas al index.html
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
 // Iniciar el servidor
